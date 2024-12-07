c582a06a8a88858d620720d32468f623fcf07fe9
Varlog Err <varlog.err@gmail.com>
2024-12-07 23:11:25 +0200

    WiP

--- dist/lib/lh-params.sh
+++ dist/lib/lh-params.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 lh_params_reset() {
-  [[ "${FUNCNAME[1]}" != _lh_params_init ]] && { "${FUNCNAME[1]}" "${@}"; return $?; }
+  [[ "${FUNCNAME[1]}" != _lh_params_init ]] && { _lh_params_init "${@}"; return $?; }
   declare vname vtype
   for vname in "${!_lh_params_map[@]}"; do
     unset "${vname}"
@@ -10,20 +10,27 @@ lh_params_reset() {
 }
 
 lh_params_set() {
-  [[ "${FUNCNAME[1]}" != _lh_params_init ]] && { "${FUNCNAME[1]}" "${@}"; return $?; }
+  [[ "${FUNCNAME[1]}" != _lh_params_init ]] && { _lh_params_init "${@}"; return $?; }
   [[ -n "${2+x}" ]] || { lh_params_noval "${1}"; return 1; }
   # shellcheck disable=SC2034
   LH_PARAMS["${1}"]="${2}"
 }
 
 lh_params_noval() {
-  [[ "${FUNCNAME[1]}" != _lh_params_init ]] && { "${FUNCNAME[1]}" "${@}"; return $?; }
+  [[ "${FUNCNAME[1]}" != _lh_params_init ]] && { _lh_params_init "${@}"; return $?; }
   LH_PARAMS_NOVAL+=("${@}")
 }
 
 lh_params_unsupported() {
   [[ "${FUNCNAME[1]}" != _lh_params_init ]] && { _lh_params_init "${@}"; return $?; }
   LH_PARAMS_UNSUPPORTED+=("${@}")
+}
+
+# lh_params_defaults \
+#   PARAM_NAME1='DEFAULT_VAL' \
+#   PARAM_NAME2!='prefix_$(!eval_expression)_${SOME_VAR}_${!PARAM_NAME1}_suffix'
+lh_params_defaults() {
+  [[ "${FUNCNAME[1]}" != _lh_params_init ]] && { _lh_params_init "${@}"; return $?; }
 }
 
 _lh_params_init() {
@@ -39,6 +46,7 @@ _lh_params_init() {
   # Ensure global variables
   declare vname vtype
   for vname in "${!_lh_params_map[@]}"; do
+    vtype="${_lh_params_map[${vname}]}"
     [[ "$(declare -p "${vname}" 2>/dev/null)" == "declare -${vtype}"* ]] && continue
 
     unset "${vname}"
