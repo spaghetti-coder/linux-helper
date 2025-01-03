91c1ccd1a02f377d1b95c4f88584996ee6413666
Spaghetti <spaghetti.coder.ru@gmail.com>
2025-01-03 13:05:16 +0200

    WiP

--- dist/short/ssh-gen-vc.sh
+++ dist/short/ssh-gen-vc.sh
@@ -730,6 +730,7 @@ is_user_privileged() { is_user_root && [[ -n "${SUDO_USER}" ]]; }
 privileged_user_home() { eval echo ~"${SUDO_USER}"; }
 
 alias_home_in_path() {
+
   declare path="${1}" home="${2:-${HOME}}"
   declare home_rex; home_rex="$(escape_sed_expr "${home%/}")"
 
@@ -743,15 +744,16 @@ is_port_valid() {
   && [[ "${1}" -le 65535 ]]
 }
 
-# USAGE:
-#   # Cache the download tool and use it
-#   declare -a DL_TOOL
-#   download_tool DL_TOOL
-#   "${DL_TOOL[@]}" https://google.com > google.txt
-#
-#   # Just use it to download
-#   download_tool https://google.com > google.txt
 download_tool() {
+  # USAGE:
+  #   # Cache the download tool and use it
+  #   declare -a DL_TOOL
+  #   download_tool DL_TOOL
+  #   "${DL_TOOL[@]}" https://google.com > google.txt
+  #
+  #   # Just use it to download
+  #   download_tool https://google.com > google.txt
+
   declare _dt_the_url
 
   if [[ "${1}" == *'://'* ]]; then
@@ -768,6 +770,56 @@ download_tool() {
 
   if [[ -n "${_dt_the_url}" ]]; then
     (set -x; "${_dt_the_tool[@]}" "${_dt_the_url}")
+  fi
+}
+
+# shellcheck disable=SC1090
+detect_os_type() {
+  # USAGE:
+  #   detect_os_type || { ERR_BLOCK }
+  # OUTPUT:
+  #   OS_ID:VERSION_ID OS_ID_LIKE:VERSION_ID_LIKE
+  # Where OS_ID_LIKE:VERSION_ID_LIKE is closest supported upstream
+  # if OS_ID is not supported
+
+  declare min_supported=(ubuntu:22.04 debian:12 centos:8 rhel:8 alpine:3.20)
+  declare -a fallback_priority=(ubuntu debian centos rhel)
+
+  [[ -n "${1+x}" ]] && [[ -n "${2+x}" ]] && {
+    # Check supported version, called in recursion
+
+    declare id="${1}" vid="${2}"
+
+    declare candidate; candidate="$(
+      printf -- '%s\n' "${min_supported[@]}" | grep -x -m 1 -- "^${id}:.*"
+    )"
+
+    printf -- '%s\n' "${candidate}" "${id}:${vid}" \
+    | sort -V | head -n 1 | grep -qFx "${candidate}"
+
+    return $?
+  }
+
+  declare SELF; SELF="${FUNCNAME[0]}"
+  declare OS_INFO; OS_INFO="$(cat /etc/os-release)" || {
+    echo "(${SELF}) Can't detect OS TYPE" >&2
+    return 1
+  }
+
+  declare id; id="$(. <(cat <<< "${OS_INFO}"); cat <<< "${ID,,}")"
+  declare vid; vid="$(. <(cat <<< "${OS_INFO}"); cat <<< "${VERSION_ID}")"
+  declare ID_VID="${id}:${vid}"
+
+  if [[ " ${min_supported[*]}" == *" ${id}:"* ]]; then
+    # Supported OS_ID
+
+    "${SELF}" "${id}" "${vid}" && {
+      echo "${ID_VID} ${ID_VID}"
+      return 0
+    }
+
+    echo "(${SELF}) Unsupported ${id} version: '${vid}'" >&2
+    return 1
   fi
 }
 # .LH_SOURCED: {{/ lib/system.sh }}
