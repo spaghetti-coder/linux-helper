f0ce32d115c2ae9e130f22675dbe2a248c5486cb
Spaghetti <spaghetti.coder.ru@gmail.com>
2025-01-21 13:35:40 +0200

    create text_fmt function

--- dist/pve/bin/deploy-lxc.sh
+++ dist/pve/bin/deploy-lxc.sh
@@ -1269,8 +1269,8 @@ download_tool() {
 }
 # .LH_SOURCED: {{/ lib/system.sh }}
 # .LH_SOURCED: {{ lib/text.sh }}
-# shellcheck disable=SC2001
-# shellcheck disable=SC2120
+# shellcheck disable=SC2001,SC2120
+
 text_ltrim() { sed -e 's/^\s\+//' <<< "${1-$(cat)}"; }
 text_rtrim() { sed -e 's/\s\+$//' <<< "${1-$(cat)}"; }
 text_trim() { text_ltrim <<< "${1-$(cat)}" | text_rtrim; }
@@ -1279,6 +1279,13 @@ text_nice() {
   text_trim <<< "${1-$(cat)}" \
   | sed -e '/^.\+$/,$!d' | tac \
   | sed -e '/^.\+$/,$!d' -e 's/^,//' | tac
+}
+text_fmt() {
+  local content; content="$(
+    sed '/[^ ]/,$!d' <<< "${1-"$(cat)"}" | tac | sed '/[^ ]/,$!d' | tac
+  )"
+  local offset; offset="$(grep '[^ ]' <<< "${content}" | grep -o '^\s*' | sort | head -n 1)"
+  sed -e 's/^\s\{0,'${#offset}'\}//' -e 's/\s\+$//' <<< "${content}"
 }
 # .LH_SOURCED: {{/ lib/text.sh }}
 # .LH_SOURCED: {{ pve/lib/lxc-do.sh }}
